23c9ae5a8a33b4e183e5b054922a434396e1d1c0
Antônio Fernandes <antonio.fernandes.bsi@gmail.com>
2022-11-07 00:44:27 -0300

    pool details, list games, send guess

---
+++ src/screens/New.tsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { Heading, VStack, Text, useToast } from "native-base";
+
+import { Header } from '../components/Header'
+import { Input } from '../components/Input'
+import { Button } from '../components/Button'
+import Logo from '../assets/logo.svg'
+import { Alert } from "react-native";
+import { api } from "../services/api";
+
+export function New() {
+  const [ title, setTitle ] = useState('')
+  const [ isLoading, setIsLoading ] = useState(false)
+  const toast = useToast();
+
+  async function handlePoolsCreate() {
+    if (!title.trim()) {
+      return toast.show({
+        title: 'Informe um nome para o seu bolão.',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
+
+    setIsLoading(true)
+
+    try {
+      await api.post('/pools', {
+        title
+      })
+
+      toast.show({
+        title: 'Bolão criado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500'
+      })
+
+        setTitle('')
+    } catch (error) {
+      return toast.show({
+        title: 'Não foi possível criar o bolão.',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <VStack
+      flex={1}
+      bgColor="gray.900">
+      <Header title="Criar novo bolão" />
+
+      <VStack
+        mt={8}
+        mx={5}
+        alignItems="center">
+        <Logo />
+
+        <Heading
+          fontFamily="heading"
+          color="white"
+          fontSize="xl"
+          my={8}
+          textAlign="center">
+          Crie seu próprio bolão da copa {'\n'} e compartilhe entre amigos!
+        </Heading>
+        
+        <Input
+          mb={2}
+          value={title}
+          onChangeText={setTitle}
+          placeholder="Qual nome do seu bolão?" />
+
+        <Button
+          title="CRIAR MEU BOLÃO"
+          isLoading={isLoading}
+          onPress={handlePoolsCreate}></Button>
+
+        <Text
+          color="gray.200"
+          fontSize="sm"
+          textAlign="center"
+          px={10}
+          mt={4}>
+          Após criar seu bolão, você receberá um código único que
+          poderá usar para convidar outras pessoas.
+        </Text>
+
+      </VStack>
+    </VStack>
+  )
+}
